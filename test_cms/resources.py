cd64019973a50a86de880db5584a3ca17522ad97
Frances Wong <fwong@lexmachina.com>
2016-10-12 15:01:49 -0700

    Add restriction on date

--- test_cms/resources.py
+++ test_cms/resources.py
@@ -1,5 +1,6 @@
 import colander
 import deform.widget
+import datetime
 
 from persistent import Persistent
 
@@ -11,8 +12,10 @@ from substanced.schema import (
     )
 from substanced.util import renamer
 
+
 def context_is_a_document(context, request):
     return request.registry.content.istype(context, 'Document')
+
 
 class DocumentSchema(Schema):
     name = NameSchemaNode(
@@ -29,11 +32,7 @@ class DocumentSchema(Schema):
 class DocumentPropertySheet(PropertySheet):
     schema = DocumentSchema()
 
-@content(
-    'Document',
-    icon='glyphicon glyphicon-align-left',
-    add_view='add_document',
-    )
+@content('Document', icon='glyphicon glyphicon-align-left', add_view='add_document',)
 class Document(Persistent):
 
     name = renamer()
@@ -42,5 +41,50 @@ class Document(Persistent):
         self.title = title
         self.body = body
 
+
+def context_is_a_blogentry(context, request):
+    return request.registry.content.istype(context, 'BlogEntry')
+
+
+class BlogEntrySchema(Schema):
+    today = datetime.date.today()
+    name = NameSchemaNode(
+        editing=context_is_a_blogentry,
+        )
+    title = colander.SchemaNode(
+        colander.String(),
+        )
+    # http://docs.pylonsproject.org/projects/deform/en/latest/api.html?highlight=date
+    # class deform.widget.DateInputWidget(*args, **kwargs)
+    # Renders a date picker widget...
+    # Most useful when the schema node is a colander.Date object.
+    date = colander.SchemaNode(
+        colander.Date(),
+        validator=colander.Range(today, None)
+    )
+    kind = colander.SchemaNode(colander.String(),
+                               validator=colander.OneOf(['a', 'b'])
+                               )
+    body = colander.SchemaNode(
+        colander.String(),
+        widget=deform.widget.RichTextWidget()
+        )
+
+class BlogEntryPropertySheet(PropertySheet):
+    schema = BlogEntrySchema()
+
+@content('BlogEntry', name='Make a blog entry, peon', icon='glyphicon glyphicon-align-left', add_view='add_blog_entry')
+class BlogEntry(Persistent):
+
+    name = renamer()
+
+    def __init__(self, title='', body='', date=''):
+        self.title = title
+        self.body = body
+        self.date = date
+
+
 def includeme(config): # pragma: no cover
     config.add_propertysheet('Basic', DocumentPropertySheet, Document)
+    config.add_propertysheet('Basic', BlogEntryPropertySheet, BlogEntry)
+
--- test_cms/views.py
+++ test_cms/views.py
@@ -1,22 +1,26 @@
 from pyramid.httpexceptions import HTTPFound
 
+from pyramid.view import(
+    view_config,
+    view_defaults,
+    )
+
 from substanced.sdi import mgmt_view
 from substanced.form import FormView
 from substanced.interfaces import IFolder
+from substanced.sdi import LEFT, RIGHT
 
-from .resources import DocumentSchema
+from .resources import (
+    DocumentSchema,
+    BlogEntrySchema
+)
 
 #
 #   SDI "add" view for documents
 #
-@mgmt_view(
-    context=IFolder,
-    name='add_document',
-    tab_title='Add Document',
-    permission='sdi.add-content',
-    renderer='substanced.sdi:templates/form.pt',
-    tab_condition=False,
-    )
+@mgmt_view(context=IFolder, name='add_document', tab_title='Add Document',
+    permission='sdi.add-content', renderer='substanced.sdi:templates/form.pt',
+    tab_condition=True, tab_near=RIGHT)
 class AddDocumentView(FormView):
     title = 'Add Document'
     schema = DocumentSchema()
@@ -24,9 +28,27 @@ class AddDocumentView(FormView):
 
     def add_success(self, appstruct):
         registry = self.request.registry
+        # import pdb; pdb.set_trace()
         name = appstruct.pop('name')
         document = registry.content.create('Document', **appstruct)
         self.context[name] = document
         return HTTPFound(
             self.request.sdiapi.mgmt_path(self.context, '@@contents')
             )
+
+@mgmt_view(context=IFolder, name='add_blog_entry', tab_title='Add Blog Entry',
+           permission='sdi.add-content', renderer='substanced.sdi:templates/form.pt',
+           tab_condition=True, tab_near=RIGHT)
+class AddBlogView(FormView):
+    title = 'Add Blog Entry'
+    schema = BlogEntrySchema()
+    buttons = ('add',)
+
+    def add_success(self, appstruct):
+        registry = self.request.registry
+        # import pdb; pdb.set_trace()
+        name = appstruct.pop('name')
+        blog_entry = registry.content.create('BlogEntry', **appstruct)
+        self.context[name] = blog_entry
+        return HTTPFound(self.request.mgmt_path(self.context, '@@contents'))
+
